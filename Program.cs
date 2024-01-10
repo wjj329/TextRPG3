f93c784a1feaf355feb2109fc540842e94abfc7a
wjj329 <wjj2187@naver.com>
2024-01-10 12:37:44 +0900

    ㅇ

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿namespace TextRPG3
+﻿
+namespace TextRPG3
 {
     // A. 팀 과제 개요
     // 캐릭터가 던전 입장 -> 전투
@@ -57,10 +58,8 @@
         Warrior,
         Mage,
         Theif,
-        Archer,
         Programmer,
     }
-
 
     public class Character
     {
@@ -70,13 +69,14 @@
         public int Exp { get; set; } // 경험치
         public int Atk { get; set; }
         public int Def { get; set; }
-        public int Speed { get; set; } // Atk, Def 처럼 레벨업에 따라 변하며, 아이템 장착 유무 및 회피 기능에 연관
+
+        //public int Speed { get; set; } // Atk, Def 처럼 레벨업에 따라 변하며, 아이템 장착 유무 및 회피 기능에 연관
         public int Hp { get; set; }
         public int Mp { get; set; }
         public int Gold { get; set; }
 
 
-        public Character(string name, JobType job, int level, int exp, int atk, int def, int speed, int hp, int mp, int gold)
+        public Character(string name, JobType job, int level, int exp, int atk, int def, int hp, int mp, int gold)
         {
             Name = name;
             Job = job;
@@ -84,13 +84,13 @@
             Exp = exp;
             Atk = atk;
             Def = def;
-            Speed = speed;
+            //Speed = speed; // 일단 pass
             Hp = hp;
             Mp = mp;
             Gold = gold;
         }
 
-        // 레벨 당 스탯(Atk,Def,Speed) 증가 메서드 구현 예정
+        // 레벨 당 스탯 증가 메서드 구현 예정
         // 
     }
 
@@ -121,29 +121,70 @@
         public int Type { get; }  // 3개로 할까 함, 무기-방어구-악세서리
         public int Atk { get; set; }
         public int Def { get; set; }
-        public int Speed { get; set; } // 아이템 스피드
         public int Gold { get; set; }
         public bool IsEquipped { get; set; } // 장착 여부
         public bool IsPurchased { get; set; } // 구매 여부
 
-        public static int ItemCnt = 0; // 아이템 종류 표시에 사용
+        public static int ItemCnt = 0; // 아이템 종류 표시에 사용, 게임 전체에 공유 됨.
 
-        public Item(string name, string description, int type, int atk, int def, int speed, int gold, bool isEquipped = false, bool isPurchased = false)
+        public Item(string name, string description, int type, int atk, int def, int gold, bool isEquipped = false, bool isPurchased = false)
         {
             Name = name;
             Desctription = description;
             Type = type;
             Atk = atk;
             Def = def;
-            Speed = speed;
             Gold = gold;
             IsEquipped = isEquipped;
             IsPurchased = isPurchased;
         }
 
-        public void PrintItemStatDescription(bool withNumber = false, int idx = 0, bool showPrice = false ) // 아이템 스탯 및 번호 출력 관련 함수
+        public void PrintItemStatDescription(bool withNumber = false, int idx = 0, bool showPrice = false ) 
+            // 아이템 스탯 및 번호 출력 관련 함수
         {
-          Console.WriteLine();
+          Console.WriteLine("- ");
+
+            if (withNumber) // 7-4 아이템 번호 컬러
+            {
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.Write("{0} ", idx);
+                Console.ResetColor();
+            }
+            if (IsEquipped) // 아이템 착용 시
+            {
+                Console.Write("[");
+                Console.ForegroundColor = ConsoleColor.Cyan; //   [ 의 뒤의 색 변경
+                Console.Write("E"); // cyan 컬러
+                Console.ResetColor(); // 색 리셋
+                Console.Write("]");
+                Console.Write(PadRightForMixedText(Name, 9)); // 장착 중인 경우는 9글자 출력, 텍스트 정렬 함수 사용
+            }
+            // 0이 아니라면 출력해라 + 삼항 연산자
+            if (Atk != 0) Console.Write($"Atk {(Atk >= 0 ? " + " : "")}{Atk}");  // 공격력이 0보다 크거나 같으면 "+" 를 붙이고 아니면 " "해라(공백).
+            if (Def != 0) Console.Write($"Def {(Def >= 0 ? " + " : "")}{Def}");
+        }
+
+        public static string PadRightForMixedText(string str, int totalLength) // 텍스트 정렬
+        {
+            int currentLength = GetPrintableLength(str); // 텍스트의 실제 길이
+            int padding = totalLength - currentLength; //  총길이 - 실제길이 = int padding 추가해야 할 길이
+            return str.PadRight(str.Length + padding); // padding 만큼 PadRight(문자열의 오른쪽)에 공백을 추가
+        }
+        public static int GetPrintableLength(string str) // 아이템 텍스트 정렬을 위해 문자별 legnth 판정 정의
+        {
+            int length = 0;
+            foreach (char c in str)
+            {
+                if (char.GetUnicodeCategory(c) == System.Globalization.UnicodeCategory.OtherLetter)
+                {
+                    length += 2; // 한글과 같은 넓은 문자는 길이를 2로 취급
+                }
+                else
+                {
+                    length += 1; // 나머지 문자는 길이를 1로 취급
+                }
+            }
+            return length;
         }
 
 
@@ -154,14 +195,39 @@
 
     internal class Program
     {
+        static Character _player;
+        static Item[] _items;
+        static Monster _monster;
+
         static void Main(string[] args)
         {
-            
+            PrintStartLogo();
+            StartMenu(); // 시작 메뉴
+            //CreatePlayerMenu(); // 캐릭터 생성 메뉴, 추후 만들면 StartMenu랑 스왑 예정, 일단 구현 X
+        }
+
+        private static void PrintStartLogo()
+        {
+            throw new NotImplementedException();
         }
 
         private static void GameDataSetting() // 게임 데이터 세팅
         {
 
         }
+
+        private static void CreatePlayerMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void StartMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
     }
 }
